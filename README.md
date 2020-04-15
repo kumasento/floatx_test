4c16438d40c34482c22a4ec7e6b6f6c366a428e6
Vincent Zhao <vincentzhaorz@gmail.com>
2020-04-15 16:52:42 +0100

    GEMM test report

---
+++ README.md
@@ -0,0 +1,20 @@
+# Integrating FloatX and Eigen
+
+
+```
+Running GEMM with size 8
+FloatX elapsed time: 0.000325024s
+float  elapsed time: 2.8502e-05s
+Running GEMM with size 32
+FloatX elapsed time: 0.0119225s
+float  elapsed time: 0.000405923s
+Running GEMM with size 64
+FloatX elapsed time: 0.0481427s
+float  elapsed time: 0.00194715s
+Running GEMM with size 128
+FloatX elapsed time: 0.392668s
+float  elapsed time: 0.0150954s
+Running GEMM with size 256
+FloatX elapsed time: 2.94766s
+float  elapsed time: 0.125947s
+```
--- main.cc
+++ main.cc
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <chrono>
 #include <floatx.hpp>
 #include <Eigen/Core>
 
@@ -6,11 +7,45 @@ typedef flx::floatx<5, 5, float> msfp11;
 
 // https://eli.thegreenplace.net/2014/sfinae-and-enable_if/
 int main(int argc, char *argv[]) {
+  typedef Eigen::Matrix<msfp11, Eigen::Dynamic, Eigen::Dynamic> Mat;
 
-  typedef Eigen::Matrix<msfp11, 4, 4> Mat;
+  const int sizes[5] = {8, 32, 64, 128, 256};
 
-  Mat A, B, C;
-  C = A * B;
+  for (int i = 0; i < 5; i ++) {
+    auto N = sizes[i];
+
+    Mat A, B, C;
+    A = Mat::Random(N, N);
+    B = Mat::Random(N, N);
+    C = Mat::Zero(N, N);
+
+    // Eigen random seems not working
+    for (int j = 0; j < N; j ++)
+      for (int k = 0; k < N; k ++) {
+        A(j, k) = static_cast<float>(rand()) / RAND_MAX - 0.5; 
+        B(j, k) = static_cast<float>(rand()) / RAND_MAX - 0.5; 
+      }
+
+    std::cout << "Running GEMM with size " << N << std::endl;
+
+    auto start = std::chrono::steady_clock::now();
+    C.noalias() = A * B;
+    auto end = std::chrono::steady_clock::now();
+    std::chrono::duration<double> elapsed_seconds = end-start;
+    std::cout << "FloatX elapsed time: " << elapsed_seconds.count() << "s\n";
+
+    // Reference floating point
+    Eigen::MatrixXf Af(N, N), Bf(N, N), Cf(N, N);
+    Af.setRandom();
+    Bf.setRandom();
+
+    start = std::chrono::steady_clock::now();
+    Cf.noalias() = Af * Bf;
+    end = std::chrono::steady_clock::now();
+    elapsed_seconds = end-start;
+    std::cout << "float  elapsed time: " << elapsed_seconds.count() << "s\n";
+  }
+
 
   return 0;
 }
